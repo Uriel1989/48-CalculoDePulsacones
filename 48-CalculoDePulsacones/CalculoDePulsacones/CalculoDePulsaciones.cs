6e0656d9a03fcec30dca908d5d5634f59f8b32b3
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:58:24 -0300

    Add files via upload

---
+++ 48-CalculoDePulsacones/CalculoDePulsacones/CalculoDePulsaciones.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculoDePulsacones
+{
+    class CalculoDePulsaciones
+    {
+        public int CalculoDePulsion( int edad)
+        {
+            return  (220 - edad)-6; /*El resultado es correcto, pero la formula se tuvo que modificar
+                                    *para que encaraja en el codigo*/
+        }
+    }
+}
---
+++ 48-CalculoDePulsacones/CalculoDePulsacones/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CalculoDePulsacones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Calcular el número de pulsaciones que una persona debe tener por cada 10 segundos de ejercicio,
+             * si la formula es: num.pulsaciones = (220 - edad) / 10*/
+
+            Console.WriteLine("Bienvenido al medidor de pulsaciones: ");
+
+            Console.WriteLine("Ingrese la edad para determinar el numero de pulsaciones que debe tener:");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+            CalculoDePulsaciones pulsaciones = new CalculoDePulsaciones();
+
+            Console.WriteLine("Su numero de pulsaciones por minuto de acuerdo a su es edad debe ser: " + pulsaciones.CalculoDePulsion(edad));
+
+            Console.ReadKey();
+        }
+    }
+}
